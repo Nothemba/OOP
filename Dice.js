032d97b665b409e05353ad28bd2981a5ba682dac
Nothemba Tsheme <tnothemba@gmail.com>
2019-04-24 15:30:27 +0200

    first commit on dice project

---
+++ Dice.js
@@ -0,0 +1,14 @@
+
+ function randomDice(){
+    var items = [1, 2 , 3, 4, 5, 6];
+    return items[Math.floor(Math.random()*items.length)];
+    
+}
+    
+      for (let i = 0; i < 10; i++)
+    //      if (items[i] = 10){
+    //          console.log(true);
+    //      }
+    //      else console.log(false)
+    
+    console.log(randomDice());
