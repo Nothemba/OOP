57da11292cb8deea66e3899ed182dfe4052f5a69
Nothemba Tsheme <tnothemba@gmail.com>
2019-07-16 11:40:01 +0200

    person exercise

---
+++ person.js
@@ -0,0 +1,15 @@
+class Person{
+   constructor(name, age, gender, interests){
+       this.name = name;
+       this. age = age;
+       this.gender = gender;
+       this.interests = interests;
+
+   }
+   sayHello(){
+       {console.log(`hello my name is ${this.name} I am ${this.age} years old and I am a ${this.gender} I love ${this.interests}`);
+   } 
+}
+}
+let person = new Person("Emihle", 2, "female", "singing");
+person.sayHello();
