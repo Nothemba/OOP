bb96eff02532336216ffabd49b5cb8e437a2f6bb
Nothemba Tsheme <tnothemba@gmail.com>
2019-07-26 17:26:21 +0200

    made some changes

--- compoundInterest/spec/BankSpec.js
+++ compoundInterest/spec/BankSpec.js
@@ -2,12 +2,12 @@ const acc = new BankAccount(1000,0.12,50,"savings")
 
 describe("Bank Account", function(){
   it("should calculate interest",function(){
-    let result = acc.calculateinterest()
+    let result = acc.calculateInterest()
     expect(result).toBe(10);
   })
 
   it("should return final balance",function(){
-    let result = acc.finalBalance();
+    let result = finalBalance();
     expect(result).toBe(960);
   })
 
